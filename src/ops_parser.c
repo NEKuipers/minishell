6e67e70522ce55611042e3ee84ed2e339ba72821
MKE <bmans@f0r2s9.codam.nl>
2020-10-13 14:35:25 +0200

    Parser/lexer for operations done, needs to be tested for robustness, doesn't include escapes yet, has 1 norme error

--- src/ops_parser.c
+++ src/ops_parser.c
@@ -6,18 +6,53 @@
 /*   By: bmans <bmans@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/08 09:58:00 by bmans         #+#    #+#                 */
-/*   Updated: 2020/10/08 14:50:36 by bmans         ########   odam.nl         */
+/*   Updated: 2020/10/13 14:29:30 by bmans         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "libft.h"
 
-t_list	*parse_args(char *line)
+static char	**list_to_arr(t_list *list)
+{
+	int		size;
+	int		i;
+	t_list	*lst;
+	char	**arr;
+
+	if (!list)
+		return (NULL);
+	lst = list;
+	size = ft_lstsize(list);
+	arr = (char **)malloc(sizeof(char *) * size + 1);
+	i = 0;
+	while (i < size)
+	{
+		arr[i] = ft_strdup(lst->content);
+		i++;
+		lst = lst->next;
+	}
+	arr[size] = NULL;
+	ft_lstclear(&list, &free);
+	return (arr);
+}
+
+static int	skip_to_quote(char *line, int index, char type)
+{
+	while (1)
+	{
+		index++;
+		if (!line[index])
+			return (-1);
+		if (line[index] == type)
+			return (index);
+	}
+}
+
+char	**parse_args(char *line)
 {
 	t_list	*list;
 	int		i;
-	char	*cptr;
 
 	i = 0;
 	list = NULL;
@@ -25,28 +60,49 @@ t_list	*parse_args(char *line)
 	{
 		while (*line == ' ')
 			line++;
-		if (line[0] == '\"' || line[0] == '\'')
+		if (!line[0])
+			break ;
+		if ((line[0] == '\"' || line[0] == '\'' || \
+			!line[i + 1] || (line[i] != ' ' && line[i + 1] == ' ')) && i > 0)
 		{
-			cptr = ft_strchr(line + 1, line[0]);
-			ft_printf("%p\n", cptr);
-			ft_lstadd_back(&list, ft_lstnew(ft_substr(line + 1, 0, line - cptr)));
-			line = cptr + 1;
-			i = -1;
-		}
-		else if (!line[i + 1] || (line[i] != ' ' && line[i + 1] == ' '))
-		{
+			if (line[0] == '\"' || line[0] == '\'')
+				i = skip_to_quote(line, i, line[0]);
 			ft_lstadd_back(&list, ft_lstnew(ft_substr(line, 0, i + 1)));
 			line += i + 1;
 			i = -1;
 		}
 		i++;
 	}
-	while (list)
+	return (list_to_arr(list));
+}
+
+static void	clear_ops(void *ops)
+{
+	char **args;
+
+	free(((t_ops *)ops)->operation);
+	args = ((t_ops *)ops)->args;
+	while (*args)
 	{
-		ft_printf("  |%s\n", (char *)(list->content));
-		list = list->next;
+		free(*args);
+		args++;
 	}
-	return (list);
+	free(args);
+	free(ops);
+}
+
+t_ops	*set_ops(char *line, int len)
+{
+	t_ops	*ops;
+
+	ops = (t_ops *)malloc(sizeof(t_ops));
+	ops->operation = ft_substr(line, 0, len);
+	ops->args = parse_args(ops->operation);
+	if (line[len + 1] == '>')
+		ops->type = '}';
+	else
+		ops->type = line[len];
+	return (ops);
 }
 
 t_list	*parse_ops(char *line)
@@ -59,14 +115,18 @@ t_list	*parse_ops(char *line)
 	list = NULL;
 	while (1)
 	{
-		if (!line[i] || ft_strchr(";|<>", line[i]))
+		if (line[i] == '\"' || line[i] == '\'')
 		{
-			ops = (t_ops *)malloc(sizeof(t_ops));
-			ops->operation = ft_substr(line, 0, i);
-			if (line[i + 1] == '>')
-				ops->type = '}';
-			else
-				ops->type = line[i];
+			i = skip_to_quote(line, i, line[i]);
+			if (i == -1)
+			{
+				ft_lstclear(&list, &clear_ops);
+				return (NULL);
+			}
+		}
+		else if (!line[i] || ft_strchr(";|<>", line[i]))
+		{
+			ops = set_ops(line, i);
 			ft_lstadd_back(&list, ft_lstnew(ops));
 			if (!line[i])
 				return (list);
@@ -80,15 +140,24 @@ t_list	*parse_ops(char *line)
 int		main(void)
 {
 	t_list	*list;
-	char	line[] = "echo \"abc ; /bin/ls | cat -e\"";
+	t_list	*tlist;
+	int		i;
+	char	line[] = "echo abc ; /bin/ls | cat -e | echo abcde";
 
 	list = parse_ops(line);
-	while (list)
+	tlist = list;
+	while (tlist)
 	{
-		ft_printf("%s\n", ((t_ops *)(list->content))->operation);
-		parse_args(((t_ops *)(list->content))->operation);
+		i = 0;
+		ft_printf("[%s]\n", ((t_ops *)(tlist->content))->operation);
+		while (((t_ops *)(tlist->content))->args[i])
+		{
+			ft_printf("[[%s]]\n", ((t_ops *)(tlist->content))->args[i]);
+			i++;
+		}
 		ft_putchar_fd(1, '\n');
-		list = list->next;
+		tlist = tlist->next;
 	}
+	while (1) {}
 	return (0);
 }
