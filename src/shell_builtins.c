549d6830ed32ae48ae2f82de33599234c187289a
Nick Kuipers <nkuipers@f0r5s9.codam.nl>
2021-10-29 16:27:00 +0200

    fixed 2 eval issues

--- src/shell_builtins.c
+++ src/shell_builtins.c
@@ -6,7 +6,7 @@
 /*   By: nkuipers <nkuipers@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/21 14:28:50 by nkuipers      #+#    #+#                 */
-/*   Updated: 2021/10/27 16:49:31 by nkuipers      ########   odam.nl         */
+/*   Updated: 2021/10/29 16:25:42 by nkuipers      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,23 +56,26 @@ int	shell_echo(char **commands)
 void	shell_exit(t_shell *shell, char **commands)
 {
 	shell->exit = 1;
-	ft_printf("exit\n");
-	if (commands[1] && commands[2])
+	if (commands[1] && ft_strisnum(commands[1]) == 0)
+	{
+		shell->rv = 255;
+		ft_printf_fd(2, "exit\nminishell:");
+		ft_printf_fd(2,"exit: %s: numeric argument required\n", commands[1]);
+	}
+	else if (commands[1] && commands[2])
 	{
 		shell->rv = 1;
 		shell->exit = 0;
-		ft_printf_fd(STDERR, "minishell: exit: too many arguments\n");
+		ft_printf_fd(STDERR, "exit\nminishell: exit: too many arguments\n");
 	}
-	else if (commands[1] && ft_strisnum(commands[1]) == 0)
-	{
-		shell->rv = 255;
-		ft_printf_fd(STDERR, "minishell: exit: %s: numeric argument required\n"\
-			, commands[1]);
-	}
-	else if (commands[1])
-		shell->rv = ft_atoi(commands[1]);
 	else
-		shell->rv = 0;
+	{
+		if (commands[1])
+			shell->rv = ft_atoi(commands[1]);
+		else
+			shell->rv = 0;
+		ft_printf_fd(0, "exit\n");
+	}
 }
 
 int	execute_builtin(char **commands, t_shell *shell)
