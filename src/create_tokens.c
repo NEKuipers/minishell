c16eeb3cc9a4ad144fdad01b87b11efeb623ecbc
MKE <tencivanni@gmail.com>
2021-11-12 15:21:35 +0100

    Tokenizer adjustments

--- src/create_tokens.c
+++ src/create_tokens.c
@@ -6,12 +6,13 @@
 /*   By: nkuipers <nkuipers@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/29 16:15:39 by nkuipers      #+#    #+#                 */
-/*   Updated: 2021/11/11 16:00:41 by bmans         ########   odam.nl         */
+/*   Updated: 2021/11/12 15:20:47 by bmans         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+/*
 int	next_alloc(char *line, int *i)
 {
 	int		count;
@@ -78,6 +79,46 @@ t_token	*next_token(char *line, int *i)
 	token->str[j] = '\0';
 	return (token);
 }
+*/
+
+int	skip_to_quote(char *str, char quote)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\\')
+			i++;
+		if (str[i] == quote)
+			return (i + 1);
+		i++;
+	}
+	return (i + 1);
+}
+
+t_token	*make_token(char *line, int *i)
+{
+	t_token	*token;
+	int		j;
+	
+	j = 0;
+	token = malloc(sizeof(t_token));
+	while (line[*i + j] && (line[*i + j] != ' '))
+	{
+//		ft_printf_fd(2, "%s\n", line + *i + j);
+//		usleep(500000);
+		if (line[*i + j] == '\\' && (line[*i + j + 1] == '\'' || line[*i + j + 1] == '\"'))
+			j++;
+		else if (line[*i + j] == '\'' || line[*i + j] == '\"')
+			j += skip_to_quote(line + *i + j + 1, line[*i + j]);
+		j++;
+	}
+//	ft_printf_fd(2, "%i %i\n", (int)ft_strlen(line + *i), j);
+	token->str = ft_substr(line, *i, j);
+	*i += j;
+	return (token);
+}
 
 void	apply_token_type(t_token *token, int separator)
 {
@@ -114,8 +155,9 @@ t_token	*create_tokens(char *line)
 	skip_space(line, &i);
 	while (line[i])
 	{
+	//	ft_printf_fd(2, "%s\n", line + i);
 		separator = find_separator(line, i);
-		token = next_token(line, &i);
+		token = make_token(line, &i);
 		token->prev = prev;
 		if (prev != NULL)
 			prev->next = token;
