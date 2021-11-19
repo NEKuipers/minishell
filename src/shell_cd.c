fa51325085d4393768be38253cabc03a8d75660a
marblekettle <tencivanni@gmail.com>
2021-11-19 11:42:51 +0100

    General errno compatibility in cd, plus Linux compatibility tweaks

--- src/shell_cd.c
+++ src/shell_cd.c
@@ -6,7 +6,7 @@
 /*   By: nkuipers <nkuipers@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/30 09:47:34 by nkuipers      #+#    #+#                 */
-/*   Updated: 2021/11/19 10:19:58 by nkuipers      ########   odam.nl         */
+/*   Updated: 2021/11/19 11:08:17 by brendan       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,10 +58,10 @@ static int	cd_fail(char *old, char *dest)
 {
 	free(old);
 	access(dest, R_OK);
-	if (errno == 13)
-		ft_printf("minishell: cd: permission denied: %s\n", dest);
-	else
-		ft_printf("minishell: cd: %s: No such file or directory\n", dest);
+//	if (errno == 13)
+		ft_printf("minishell: cd: %s: %s\n", dest, strerror(errno));
+//	else
+//		ft_printf("minishell: cd: %s: No such file or directory\n", dest);
 	return (1);
 }
 
